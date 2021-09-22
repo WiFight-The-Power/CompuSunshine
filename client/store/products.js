edc714341391e300d015bd9af9c30dc6a900aead
Clay Carroll <claycarroll7@gmail.com>
2021-09-22 14:46:12 -0500

    feat(store/products): added redux for all products

---
+++ client/store/products.js
@@ -0,0 +1,30 @@
+import axios from "axios"
+
+//ACTION TYPES
+const SET_PRODUCTS = "SET_PRODUCTS";
+
+//ACTION CREATORS
+const _setProducts = (products) => {
+    return {
+        type: SET_PRODUCTS,
+        products
+    }
+}
+
+//THUNK
+export const fetchProducts = () => {
+    return async (dispatch) => {
+        const {data: products} = await axios.get("/api/products");
+        dispatch(_setProducts(products));
+    }
+}
+
+//REDUCER
+export default function productsReducer(state = [], action) {
+    switch(action.type) {
+        case SET_PRODUCTS:
+            return action.products;
+        default: 
+            return state;
+    }
+}
