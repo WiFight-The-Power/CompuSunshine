9eaa34043bbe5daa726fb3abf8c31f013539e7cc
ApollosSevere <severeapollos@gmail.com>
2021-09-23 17:58:05 -0400

    feat(addProduct)

--- client/store/product.js
+++ client/store/product.js
@@ -1,34 +1,48 @@
-import axios from 'axios'
+import axios from "axios";
 
 //ACTION
-const SET_PRODUCT = 'SET_PRODUCT'
+const SET_PRODUCT = "SET_PRODUCT";
+// const ADD_PRODUCT = "ADD_PRODUCT";
 
 //ACTION CREATOR
-export const _setProduct = product => {
+export const _setProduct = (product) => {
   return {
     type: SET_PRODUCT,
     product,
-  }
-}
+  };
+};
 
 //THUNK
-export const fetchProduct = productId => {
-  return async dispatch => {
+export const fetchProduct = (productId) => {
+  return async (dispatch) => {
     try {
-      const { data: product } = await axios.get(`/api/products/${productId}`)
-      dispatch(_setProduct(product))
+      const { data: product } = await axios.get(`/api/products/${productId}`);
+      dispatch(_setProduct(product));
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-}
+  };
+};
+
+//THUNK
+export const setProduct = (productId, loggedInUser, price) => {
+  return async (dispatch) => {
+    try {
+      const obj = { productId, loggedInUser, price };
+      const { data: product } = await axios.post(`/api/products/${1}`, obj);
+      console.log(product);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
 
 //REDUCER
 export default function productReducer(state = {}, action) {
   switch (action.type) {
     case SET_PRODUCT:
-      return action.product
+      return action.product;
     default:
-      return state
+      return state;
   }
 }
