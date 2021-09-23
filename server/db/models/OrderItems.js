7c8d2cb59854a6c4551c9a436358de4460061e5a
Jean Carlos Zhong <64503277+jeancarlosZ@users.noreply.github.com>
2021-09-23 16:05:39 -0400

    fix(server/db/models: changed type to ENUM

--- server/db/models/OrderItems.js
+++ server/db/models/OrderItems.js
@@ -2,13 +2,14 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 module.exports = db.define('orderitem', {
-    // quantity: {
-    //     type: Sequelize.INTEGER,
-    // },        We will work off of each instance
-    ordered: {
-        type: Sequelize.BOOLEAN
-    },
-    price: {
-        type: Sequelize.INTEGER
-    }
+  // quantity: {
+  //     type: Sequelize.INTEGER,
+  // },        We will work off of each instance
+  status: {
+    type: Sequelize.ENUM,
+    values: ['pending', 'purchased'],
+  },
+  price: {
+    type: Sequelize.INTEGER,
+  },
 })
